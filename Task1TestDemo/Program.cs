6bb4678e14a4428bd990a8d076e455611eb80da5
Alexander <continenttal2015@yandex.ru>
2023-04-25 18:59:48 +0300

    Add Task1Demo

---
+++ Task1TestDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Clear();
+if (DateTime.Now.Hour < 12)
+{
+    Console.WriteLine("Доброк утро");
+}
+else
+{
+    Console.WriteLine("Добрый вечер");
+}
+Console.WriteLine("Введите, пожалуйста, ваше имя и нажмите Enter");
+string name=Console.ReadLine();
+Console.WriteLine("Введите, пожалуйста, название блюда и нажмите Enter");
+string order =Console.ReadLine();
+Console.WriteLine("Спасибо за Ваш заказ, {0}! Вы выбрали {1}", name, order);
+int time = new Random().Next(5,20);
+Console.WriteLine("Ваш заказ будет котов через {0} минут", time);
