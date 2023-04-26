fb0cd1df233b731693676af053c143b4abd33a29
Alexander <continenttal2015@yandex.ru>
2023-04-26 17:33:18 +0300

    variables

---
+++ Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿// тип_данных название_переменной
+int x=17;
+string title="Война и мир";
+bool jobIsDone;
+// Выполняется сложная вычислительная операуия
+jobIsDone=true;
+// Console.WriteLine(x);
+// Console.WriteLine(title);
+// Console.WriteLine(jobIsDone);
+x=105;
+int y=22;
+x=y;
+Console.WriteLine(x);
